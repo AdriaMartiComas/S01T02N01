1dbe12a80f3f68c8a323d98394f8185448cd4f39
adriamarticomas <adriamarticomas@192.168.1.135>
2022-07-14 16:32:41 +0200

    nivell 1 fet i arreglat

--- S01T02N01MartiComasAdria/src/n1Exercici1/n1Exercici1.java
+++ S01T02N01MartiComasAdria/src/n1Exercici1/n1Exercici1.java
@@ -1,30 +1,34 @@
 package n1Exercici1;
 
-import java.util.ArrayList;
-
 public class n1Exercici1 {
 
 	public static void main(String[] args) {
 
 		Venda venda1 = new Venda();
-		
-		//Excepció personalitzada VendaBuidaException 
+
+		// Excepció personalitzada VendaBuidaException
 		venda1.calcularTotal(venda1.getProductes());
 		System.out.println(venda1);
-		
+
 		venda1.getProductes().add(new Producte("Arròs", 1.7));
 		venda1.getProductes().add(new Producte("Pa", 2.5));
 		venda1.getProductes().add(new Producte("Suc", 3));
-	
-		//Metode funciona correctament
+
+		// Metode funciona correctament
 		System.out.println();
 		venda1.calcularTotal(venda1.getProductes());
 		System.out.println(venda1);
-		
+
 		// ArrayIndexOutOfBoundsException
-		venda1.getProductes().get(3);
+		System.out.println();
+		try {
+			venda1.getProductes().get(3);
+
+		} catch (IndexOutOfBoundsException e) {
+			System.out.println("Error! La posició buscada no existeix");
+
+		}
 
 	}
-	
 
 }
